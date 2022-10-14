1777a454b1d9ec0682c4d63dfe0c082b2057e7c5
Andrea Cimmino Arriaga <cimmino@fi.upm.es>
2022-10-14 16:14:02 +0200

    v0.1.2 filtering is namespace aware

--- src/main/java/handlers/XmlHandler.java
+++ src/main/java/handlers/XmlHandler.java
@@ -1,30 +1,20 @@
 package handlers;
 
-import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathEvaluationResult;
 import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import javax.xml.xpath.XPathNodes;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -52,21 +42,23 @@ import helio.blueprints.DataHandler;
  */
 public class XmlHandler implements DataHandler {
 
-	private static final long serialVersionUID = 1L;
 	Logger logger = LoggerFactory.getLogger(XmlHandler.class);
 	private static final String CONFIGURATION_KEY = "iterator";
 	private String iterator;
-	private 	final XPath XPATH = XPathFactory.newInstance().newXPath();
+	private final XPath XPATH = XPathFactory.newInstance().newXPath();
 
 	/**
-	 * This constructor creates an empty {@link XmlHandler} that will need to be configured using a valid {@link JsonObject}
+	 * This constructor creates an empty {@link XmlHandler} that will need to be
+	 * configured using a valid {@link JsonObject}
 	 */
 	public XmlHandler() {
 		super();
 	}
 
 	/**
-	 * This constructor instantiates a valid {@link XmlHandler} with the provided iterator
+	 * This constructor instantiates a valid {@link XmlHandler} with the provided
+	 * iterator
+	 * 
 	 * @param iterator a valid XPath expression
 	 */
 	public XmlHandler(String iterator) {
@@ -75,17 +67,19 @@ public class XmlHandler implements DataHandler {
 
 	@Override
 	public List<String> iterator(String dataChunk) {
-		List<String> queueOfresults = new ArrayList();
-		if(dataChunk != null) {
+		List<String> queueOfresults = new ArrayList<>();
+		if (dataChunk != null) {
 			try {
-				Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(dataChunk));
+				Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+						.parse(new InputSource(dataChunk));
 				// 3. Compile XPath
-				XPathExpression expr =  XPATH.compile(iterator);
+				XPathExpression expr = XPATH.compile(iterator);
+
 				// 4. Evaluate XPath in the document
 				NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-				if(nodes.getLength()>0) {
+				if (nodes.getLength() > 0) {
 					// 5. Transform each resultant node into a string xml document
-					for(int index=nodes.getLength()-1; index >= 0 ; index--) {
+					for (int index = nodes.getLength() - 1; index >= 0; index--) {
 						Node node = nodes.item(index);
 						Document subXmlDocument = node.getOwnerDocument();
 						DOMImplementationLS domImplLS = (DOMImplementationLS) subXmlDocument.getImplementation();
@@ -93,10 +87,10 @@ public class XmlHandler implements DataHandler {
 						String stringXmlDocument = serializer.writeToString(node);
 						queueOfresults.add(stringXmlDocument);
 					}
-				}else {
+				} else {
 					logger.warn("Given xPath expression does not match in the document");
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				logger.warn(e.toString());
 			}
 		}
@@ -108,33 +102,34 @@ public class XmlHandler implements DataHandler {
 		List<String> results = new ArrayList<>();
 		try {
 			// 2. Create XML document
-			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-					.parse(new InputSource(new StringReader(dataChunk)));
+			DocumentBuilderFactory builder = DocumentBuilderFactory.newInstance();
+			builder.setNamespaceAware(true);
+
+			Document doc = builder.newDocumentBuilder().parse(new InputSource(new StringReader(dataChunk)));
 			// 3. Compile XPath
 			XPathExpression expr = XPATH.compile(filter);
 			// 3. Evaluate XPath in the document
-			results = extractInformation( expr,  doc,  filter,  dataChunk);
+			results = extractInformation(expr, doc, filter, dataChunk);
 		} catch (Exception e) {
 			logger.error(e.toString());
 		}
 		return results;
 	}
-	
-	public List<String> extractInformation(XPathExpression expr, Document doc, String filter, String dataChunk){
+
+	public List<String> extractInformation(XPathExpression expr, Document doc, String filter, String dataChunk) {
 		List<String> results = new ArrayList<>();
-		try {		
-			
+		try {
+
 			NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-			if(nodes.getLength()==0) {
+			if (nodes.getLength() == 0) {
 				String node = (String) expr.evaluateExpression(doc, String.class);
-				System.out.println(node);
 				results.add(node);
-			}else {
-				for(int index=0; index <nodes.getLength(); index++)
-					results.add(nodeToString(nodes.item(index)));
+			} else {
+				for (int index = 0; index < nodes.getLength(); index++)
+					results.add(nodeToString(nodes.item(index), doc));
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 //		if (nodes.getLength() == 0) {
@@ -144,32 +139,34 @@ public class XmlHandler implements DataHandler {
 //				results.add(nodes.item(0).getTextContent());
 //
 //		}
-		
+
 		return results;
 	}
 
-	 private  String nodeToString(Node node) {
-		    StringWriter sw = new StringWriter();
-		    try {
-		      Transformer t = TransformerFactory.newInstance().newTransformer();
-		      t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		      t.setOutputProperty(OutputKeys.INDENT, "yes");
-		      t.transform(new DOMSource(node), new StreamResult(sw));
-		    } catch (TransformerException te) {
-		    	te.printStackTrace();
-		      System.out.println("nodeToString Transformer Exception");
-		    }
-		    return sw.toString();
-		  }
-	
+	private String nodeToString(Node node, Document doc) {
+		StringWriter sw = new StringWriter();
+		try {
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+			t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			t.setOutputProperty(OutputKeys.INDENT, "yes");
+			t.transform(new DOMSource(node), new StreamResult(sw));
+		} catch (TransformerException te) {
+			te.printStackTrace();
+			System.out.println("nodeToString Transformer Exception");
+		}
+		return sw.toString();
+	}
+
 	@Override
 	public void configure(JsonObject configuration) {
-		if(configuration.has(CONFIGURATION_KEY)) {
+		if (configuration.has(CONFIGURATION_KEY)) {
 			iterator = configuration.get(CONFIGURATION_KEY).getAsString();
-			if(iterator.isEmpty())
-				throw new IllegalArgumentException("XmlHandler needs to receive non empty value for the keey 'iterator'");
-		}else {
-			throw new IllegalArgumentException("XmlHandler needs to receive json object with the mandatory key 'iterator'");
+			if (iterator.isEmpty())
+				throw new IllegalArgumentException(
+						"XmlHandler needs to receive non empty value for the keey 'iterator'");
+		} else {
+			throw new IllegalArgumentException(
+					"XmlHandler needs to receive json object with the mandatory key 'iterator'");
 		}
 	}
 
